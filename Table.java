a2192e6b8ba717bcf7ed8c68cc655b5e56c1f47b
ncriem <48286082+ncriem@users.noreply.github.com>
2019-04-25 09:36:10 -0400

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,15 +1,15 @@
 import java.util.ArrayList;
 
-public class Table {
+public class Table extends Banquet{
     int numTable = 10;
     int tablePeople = 10;
-    int numCompanies=16;
+    int numCompanies = 16;
 
-    Attendee tables[][] = new ArrayList<Attendee>[numTable][tablePeople];
+    Attendee tables[][] = new Attendee[numTable][tablePeople];
 
     public void printTable(int tableNum) {
         for (int i = 0; i < tablePeople; i++) {
-            tables[tableNum][tablePeople].print();
+            System.out.println(tables[tableNum][tablePeople]);
         }
     }
 
@@ -30,40 +30,36 @@ public class Table {
         }
         return true;
     }
-/*
-    public void populate(int josh[][], Attendee guests[][]) { //josh array shows comapny sizes, guests array has all attendees
-        int max = 0;
-        for (int i = 0; i < numCompanies; i++) {
-            for (int j = 0; j < numCompanies; j++) {//finds company with max people
-                if (josh[j][1] > max &&josh[j][0]!=0) max = j;
-            }
-            if(josh[max][1]<=tablePeople){ //if all the people in the company can sit at a table
-                for(Attendee person:guests)//for each person in the guests lists
-                    if(guests[numTable][tablePeople].returnCompany()==max) {//if a person is part of the "max" company
-                        tables[max].add(person);//adds person to the company table arraylist
+
+
+    public void populate(int josh[], ArrayList<Attendee>attendees) { //josh array shows comapny sizes, guests array has all attendees
+
+        for(int i=0;i<josh.length;i++){
+            if(josh[i]==numTable){
+                for(Attendee person:attendees){
+                    int counter=0;
+                    if(person.returnCompany().equals(i)){
+                        tables[i][counter]=person;
+                        counter++;
                     }
+                }
             }
-            josh[max][0]=0;//sets the company id in josh array to zero so it won't be chosen as max again
+            if(josh[i]<numTable){
+                for(Attendee person:attendees){
+                    if(person.returnComany().equals(i)){
+                        for(int j=0;j<numTable;j++){
+                            for(int z=0;z<tablePeople;z++){
+                                if(tables[j][z]==null){
+                                    tables[j][z]=person;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
+
     }
 }
-*/
-    
-public void populate(int josh[][], Attendee guests[][]) { //josh array shows comapny sizes, guests array has all attendees
 
-//sort companies from biggest to smallest <--Manav
-  for(int company: companyArrayList{  
-//populate companies with size=10 first <--Chase
 
-//work from big to small when populating the other companies <--Nathan
-      if(companySize<tablePeople){//if the company size is less than the number of seats at a table
-      for(attendee person: guests){
-          if(attendee.getCompany==company){
-              for(String seat: tables){
-                  if(seat==null)seat=attendee.returnName();
-              }//for
-          }//if
-      }//for
-  }//if
-  }//big for
-}//method
